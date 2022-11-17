3388e02fd4f5d4e36359f4fc9c286c7be48e2d6f
Asmolov Sergey <asmolovasmolovasmolov@mail.ru>
2022-11-18 01:53:51 +0300

    Добавлена функция удаления пользователя

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -16,6 +16,30 @@ const getAllUsers = async (req, res) => {
     }
 }
 
+const deleteUser = async (req, res) => {
+    try {
+
+        const {login} = req.body
+        const user = await User.findOne({where : {login}})
+        if (!user) {
+            return res.status(406).json({
+                message:'Пользователя с таким логином не существует.'
+            })
+        }
+
+        await User.destroy({where: {id: user.id}, force:true})
+
+        return res.status(200).json({
+            message:'Пользователь успешно удалён.',
+        })
+    } catch (error) {
+        res.status(408).json({
+            message:'При удалении пользователя произошла непредвиденная ошибка.'
+        })
+    }
+}
+
 module.exports = {
     getAllUsers,
+    deleteUser,
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const checkRole = require('../middleware/checkRoleMiddleware')
-const { getAllUsers } = require('../controllers/user')
+const { getAllUsers, deleteUser } = require('../controllers/user')
 
 const router = new Router()
 
 router.get('/', checkRole('ADMIN'), getAllUsers)
+
+router.delete('/delete', checkRole('ADMIN'), deleteUser)
 
 module.exports = router
