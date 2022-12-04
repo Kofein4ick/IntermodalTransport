139414453fc40d73e3aa67b0daca6ed324b74411
ArtemSmirn <artyom.artem2000@yandex.ru>
2022-12-04 13:38:25 +0300

    check git

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -10,7 +10,7 @@ export default function AdminPage() {
     const dispatch = useDispatch()
     const users = useSelector((state) => state.user.users)
 
-    useEffect(()=> {
+    useEffect(()=> { 
     dispatch(getAllUsers())
     },[dispatch])
 
